05d2231ed982d39f84129aae20ba70949cdf91df
santiagoarevalo <santiguitar2003@gmail.com>
2021-04-16 15:41:02 -0500

    Implemented output printing by file and simplified Hanoi method

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 	private static int contA = 0;
 	private static int contB = 0;
 	private static int contC = 0;
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static BufferedReader br;
+	static BufferedWriter bw;
 	
 	private static void towersCont(int A, int C) {
 		if(A == 1 && C == 2) {
@@ -38,20 +39,17 @@ public class Main {
 	}
 	
 	public static void Hanoi(int n, int A,  int B, int C) throws IOException {
-		if(n == 1) {
-			towersCont(A, C);
-			bw.write(Integer.toString(contA) + Integer.toString(contB) + Integer.toString(contC) + "\n");
-		}
-		else {
+		if(n > 0) {
 		    Hanoi(n-1, A, C, B);
 		    towersCont(A, C);
-		    bw.write(Integer.toString(contA) + Integer.toString(contB) + Integer.toString(contC) + "\n");
+		    bw.write(Integer.toString(contA) + " " + Integer.toString(contB) + " " + Integer.toString(contC) + "\n");
 		    Hanoi(n-1, B, A, C);
 		}
 	}
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br = new BufferedReader(new FileReader("data/Hanoi_input.txt"));
+		bw = new BufferedWriter(new FileWriter("data/Hanoi_output.txt"));
 		
 		int m = Integer.parseInt(br.readLine());
 		for(int i = 0; i<m; i++) {
@@ -59,7 +57,7 @@ public class Main {
 			contA = nDisks;
 			contB = 0;
 			contC = 0;
-			bw.write(Integer.toString(nDisks) + "0" + "0\n");
+			bw.write(Integer.toString(nDisks) + " " + "0" + " " + "0\n");
 			Hanoi(nDisks, 1, 2, 3);
 			bw.newLine();
 		}
